ad36d82fa13077c41b60bca7089befee9ab2878d
moskaleva2010 <122081575+moskaleva2010@users.noreply.github.com>
2023-01-06 10:47:40 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1672990647,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ночнойгород.png",
+		"images/стена.png",
+		"images/ковбой-sheet0.png",
+		"images/блок-sheet0.png",
+		"images/blok-sheet0.png",
+		"images/coin-sheet0.png",
+		"images/coins-sheet0.png",
+		"images/портал-sheet0.png",
+		"images/портал-sheet1.png",
+		"images/портал-sheet2.png",
+		"images/шипы-sheet0.png",
+		"images/фон.png",
+		"images/трава.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
